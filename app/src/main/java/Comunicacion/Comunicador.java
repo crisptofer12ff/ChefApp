7d8551073db2795236cf39acfd04109ce226f98c
Crisptofer Fernández Fernández <alberto12ff@gmail.com>
2016-10-29 08:40:19 -0600

    actualizacion

--- app/src/main/java/Comunicacion/Comunicador.java
+++ app/src/main/java/Comunicacion/Comunicador.java
@@ -1,36 +1,26 @@
 package Comunicacion;
 
 import android.content.Context;
-import android.net.*;
-import android.net.http.AndroidHttpClient;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
-import org.apache.http.util.EntityUtils;
+;
 
 /**
  * Created by erick on 10/26/2016.
  */
 public class Comunicador {
 
-    static InputStream GET(String direccionURL) throws IOException {
+    public static InputStream GET(String direccionURL) throws IOException {
         URL url = new URL(direccionURL);
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
         InputStream in;
@@ -42,7 +32,7 @@ public class Comunicador {
         return in;
     }
 
-    static void POST(String direccionURL, String JSON) throws IOException {
+    public static void POST(String direccionURL, String JSON) throws IOException {
         String urlParameters  = "param1=" + URLEncoder.encode(JSON, "UTF-8");
         byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8 );
         URL url = new URL(direccionURL);
@@ -62,7 +52,7 @@ public class Comunicador {
 
     }
 
-    static boolean verificarConexion(Context contexto) {
+    public static boolean verificarConexion(Context contexto) {
 
         ConnectivityManager connMgr = (ConnectivityManager)
                 contexto.getSystemService(Context.CONNECTIVITY_SERVICE);
